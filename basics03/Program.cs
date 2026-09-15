2c0409c9a736dbf0fdd0c53d9b09980f33a46264
menna <mennaayman200004@gmail.com>
2026-09-16 02:56:45 +0300

    Add project files.

---
+++ basics03/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace basics03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print the second price(index 1).
+            
+            double[] array1 = [25.5, 40.0, 33.75];
+            Console.WriteLine(array1[1]);
+
+            #endregion
+
+
+
+
+        }
+    }
+}
