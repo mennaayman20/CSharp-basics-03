f15d00d312f4b8d776c1943788dfe36a4c2c3753
menna <mennaayman200004@gmail.com>
2026-09-16 03:03:59 +0300

    Create a 2x2 multidimensional array

--- basics03/Program.cs
+++ basics03/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace basics03
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace basics03
 {
     internal class Program
     {
@@ -10,6 +12,14 @@
             Console.WriteLine(array1[1]);
 
             #endregion
+
+
+            #region Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and shelf 1 has 1, 4 copies.Print the number of copies on shelf 1, slot 0.
+            int[,] shelfCopies = {{3, 5}, {1, 4}};
+            Console.WriteLine(shelfCopies[1, 0]);
+
+            #endregion
+
 
 
 
